681ed3c1f4842b5707a7c146f7074a557061cba5
ZhekoIlchev <Jeko.Ilchev@gmail.com>
2021-12-16 21:51:01 +0200

    Lab: Problem_01_DayOfWeek

---
+++ 2. Java Fundamentals/3-Arrays/src/Problem_01_DayOfWeek.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Problem_01_DayOfWeek {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] dayNames = new String[]{"Monday", "Tuesday", "Wednesday", "Thursday",
+                "Friday", "Saturday", "Sunday"};
+
+        int input = Integer.parseInt(scanner.nextLine());
+
+        if (1 <= input && input <= 7) {
+            System.out.println(dayNames[input - 1]);
+        } else {
+            System.out.println("Invalid day");
+        }
+    }
+}
