bc044439676c97bc9ecac9c34d474e5d65669277
ZhekoIlchev <Jeko.Ilchev@gmail.com>
2022-01-13 18:26:59 +0200

    Lab: Problem_04_SayHelloExtended

---
+++ 3. Java Advanced/2. Java OOP/4-Interfaces-and-Abstraction/src/Lab_Problem_04_SayHelloExtended/BasePerson.java
@@ -0,0 +1,18 @@
+package Lab_Problem_04_SayHelloExtended;
+
+public abstract class BasePerson implements Person {
+    private String name;
+
+    protected BasePerson(String name) {
+        this.setName(name);
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ 3. Java Advanced/2. Java OOP/4-Interfaces-and-Abstraction/src/Lab_Problem_04_SayHelloExtended/Person.java
@@ -0,0 +1,9 @@
+package Lab_Problem_04_SayHelloExtended;
+
+public interface Person {
+
+    String getName();
+
+    String sayHello();
+
+}
