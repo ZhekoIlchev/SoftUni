97e14fdc52cd74e0ac00d0cf17629941beece5df
Zheko <Jeko.Ilchev@gmail.com>
2022-06-08 23:08:48 +0300

    Lab: MiniORM

---
+++ 4. Spring Data/2-ORM-Fundamentals-Lab/src/main/java/Main.java
@@ -0,0 +1,25 @@
+import entities.User;
+import orm.EntityManager;
+import orm.MyConnector;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter user: ");
+        String user = scanner.nextLine();
+
+        System.out.println("Enter password: ");
+        String password = scanner.nextLine();
+
+        MyConnector.createConnection(user, password, "custom-orm");
+        Connection connection = MyConnector.getConnection();
+
+        EntityManager<User> entityManager = new EntityManager<>(connection);
+    }
+}
