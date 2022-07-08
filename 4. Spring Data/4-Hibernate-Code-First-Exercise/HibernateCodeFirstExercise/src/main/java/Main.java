6edbfde86c8cbcebb659d234ab77f5445a61e0c2
Zheko <Jeko.Ilchev@gmail.com>
2022-07-08 18:41:13 +0300

    Exercise: Hibernate Code First

--- 4. Spring Data/4-Hibernate-Code-First-Exercise/HibernateCodeFirstExercise/src/main/java/Main.java
+++ 4. Spring Data/4-Hibernate-Code-First-Exercise/HibernateCodeFirstExercise/src/main/java/Main.java
@@ -1,5 +1,10 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
-        
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("cfa");
+        EntityManager entityManager = emf.createEntityManager();
     }
 }
