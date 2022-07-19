3de938160e4dec22dcbf41e0b885cc064606d304
Zheko <Jeko.Ilchev@gmail.com>
2022-07-19 23:52:57 +0300

    Exercise: Hibernate Code First

---
+++ 4. Spring Data/4-Hibernate-Code-First-Exercise/HibernateCodeFirstExercise/src/main/java/entity/Sale.java
@@ -0,0 +1,49 @@
+package entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name = "sales")
+public class Sale extends BaseEntity{
+    private Product product;
+    private Customer customer;
+    private StoreLocation storeLocation;
+    private Date date;
+
+    public Sale() {
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public StoreLocation getStoreLocation() {
+        return storeLocation;
+    }
+
+    public void setStoreLocation(StoreLocation storeLocation) {
+        this.storeLocation = storeLocation;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
