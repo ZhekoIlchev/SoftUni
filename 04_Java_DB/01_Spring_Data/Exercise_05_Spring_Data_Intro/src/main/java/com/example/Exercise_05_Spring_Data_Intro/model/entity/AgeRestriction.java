883f596464d724bda55ccf1127e755a467e21bdd
ZhekoIlchev <jeko.ilchev@gmail.com>
2024-03-24 23:43:53 +0200

    Remove Exercise 05.

--- 04_Java_DB/01_Spring_Data/Exercise_05_Spring_Data_Intro/src/main/java/com/example/Exercise_05_Spring_Data_Intro/model/entity/AgeRestriction.java
+++
@@ -1,5 +0,0 @@
-package com.example.Exercise_05_Spring_Data_Intro.model.entity;
-
-public enum AgeRestriction {
-    MINOR, TEEN, ADULT
-}
--- 04_Java_DB/01_Spring_Data/Exercise_05_Spring_Data_Intro/src/main/java/com/example/Exercise_05_Spring_Data_Intro/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Exercise_05_Spring_Data_Intro.repository;
-
-import com.example.Exercise_05_Spring_Data_Intro.model.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
