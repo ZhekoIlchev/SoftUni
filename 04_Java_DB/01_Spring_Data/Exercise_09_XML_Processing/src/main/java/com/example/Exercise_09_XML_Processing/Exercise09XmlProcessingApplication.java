08c82276df7131820a94b7b0bf7c6a335a335aab
ZhekoIlchev <jeko.ilchev@gmail.com>
2024-04-21 20:39:20 +0300

    Add Exercise09XmlProcessingApplication.class

---
+++ 04_Java_DB/01_Spring_Data/Exercise_09_XML_Processing/src/main/java/com/example/Exercise_09_XML_Processing/Exercise09XmlProcessingApplication.java
@@ -0,0 +1,12 @@
+package com.example.Exercise_09_XML_Processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise09XmlProcessingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise09XmlProcessingApplication.class, args);
+    }
+}
