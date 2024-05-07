74951e1a3f0c3137efa3d067bd5e517e3fc7ff29
ZhekoIlchev <jeko.ilchev@gmail.com>
2024-05-07 23:09:39 +0300

    Add Product.class

---
+++ 04_Java_DB/01_Spring_Data/Exercise_09_XML_Processing/src/main/java/com/example/Exercise_09_XML_Processing/model/entity/Product.java
@@ -0,0 +1,68 @@
+package com.example.Exercise_09_XML_Processing.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+@Entity
+@Table(name = "products")
+public class Product extends BaseEntity {
+    @Column(name = "name")
+    private String name;
+    @Column(name = "price")
+    private BigDecimal price;
+    @ManyToOne
+    private User seller;
+    @ManyToOne
+    private User buyer;
+    @ManyToMany
+    private Set<Category> categories;
+
+    public Product() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public User getSeller() {
+        return seller;
+    }
+
+    public void setSeller(User seller) {
+        this.seller = seller;
+    }
+
+    public User getBuyer() {
+        return buyer;
+    }
+
+    public void setBuyer(User buyer) {
+        this.buyer = buyer;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
+    }
+}
