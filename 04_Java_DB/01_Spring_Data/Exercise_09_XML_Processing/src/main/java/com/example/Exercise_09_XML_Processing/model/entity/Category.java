574ef0cce1ec3616e9b104d56925018ed572f76f
ZhekoIlchev <jeko.ilchev@gmail.com>
2024-05-08 22:23:38 +0300

    Add Category.class

---
+++ 04_Java_DB/01_Spring_Data/Exercise_09_XML_Processing/src/main/java/com/example/Exercise_09_XML_Processing/model/entity/Category.java
@@ -0,0 +1,38 @@
+package com.example.Exercise_09_XML_Processing.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "categories")
+public class Category extends BaseEntity {
+    @Column(name = "name", length = 15, nullable = false, unique = true)
+    private String name;
+
+    public Category() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(name, category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
