2c7d185edd0bb2f3a118f924a11a1aeec1b13fec
ZhekoIlchev <jeko.ilchev@gmail.com>
2024-05-23 23:36:09 +0300

    Add GlobalConstants.class

---
+++ 04_Java_DB/01_Spring_Data/Exercise_09_XML_Processing/src/main/java/com/example/Exercise_09_XML_Processing/constants/GlobalConstants.java
@@ -0,0 +1,11 @@
+package com.example.Exercise_09_XML_Processing.constants;
+
+public class GlobalConstants {
+    public static final String RESOURCE_FILE_PATH = "src/main/resources/files/";
+    public static final String CATEGORIES_FILE_NAME = "categories.xml";
+    public static final String USERS_FILE_NAME = "users.xml";
+    public static final String PRODUCTS_FILE_NAME = "products.xml";
+    public static final String OUTPUT_FILE_PATH = "src/main/resources/files/out/";
+    public static final String PRODUCTS_IN_RANGE_FILE_NAME = "products-in-range.xml";
+    public static final String USERS_WITH_SOLD_PRODUCTS = "users-sold-products.xml";
+}
