8b7275b8a2426c88d9716e13447d0c3f87057214
ZhekoIlchev <jeko.ilchev@gmail.com>
2024-05-26 23:45:46 +0300

    Exercise 09: XML Processing.

---
+++ 04_Java_DB/01_Spring_Data/Exercise_09_XML_Processing/src/test/java/com/example/Exercise_09_XML_Processing/Exercise09XmlProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Exercise_09_XML_Processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise09XmlProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
