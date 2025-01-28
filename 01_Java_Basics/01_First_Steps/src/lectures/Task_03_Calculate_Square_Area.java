25a0988f72ddfe59d4b349c940eea067e7659f17
ZhekoIlchev <jeko.ilchev@gmail.com>
2025-01-28 23:38:20 +0200

    Java Basics - First Steps.

--- 01_Java_Basics/01_First_Steps/src/lectures/Task_03_Calculate_Square_Area.java
+++ 01_Java_Basics/01_First_Steps/src/lectures/Task_03_Calculate_Square_Area.java
@@ -2,7 +2,7 @@ package lectures;
 
 import java.util.Scanner;
 
-public class Task_3_Calculate_Square_Area {
+public class Task_03_Calculate_Square_Area {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int side = Integer.parseInt(scanner.nextLine());
